3c64ec7f5bb439b452dc68dea7ebe67b8fe8ef97
lucas-paz <lucas.hpaz@outlook.com>
2021-09-04 16:02:49 -0300

    cria handler de erros generico

---
+++ Cep/ErrorMiddleware.js
@@ -0,0 +1,11 @@
+const handleErrors = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(400).json({ error: { message: err.details[0].message } })
+  };
+
+  return res.status(err.code).json({ message: err.message })
+};
+
+module.exports = {
+  handleErrors
+};
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // Importando as ferramentas globais
 const express = require('express');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
+const validator = require('express-joi-validation').createValidator({});
+
+const bodySchema = Joi.object({
+  cep: Joi.string().pattern(/\d{5}-\d{3}/).required(),
+  logradouro: Joi.string().not().empty().required().max(50),
+  bairro: Joi.string().not().empty().required().max(20),
+  localidade: Joi.string().not().empty().required().max(20),
+  uf: Joi.string().not().empty().required().max(2),
+});
 
 // Comecando a aplicacao
 const app = express();
