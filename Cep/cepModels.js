975fcf6993f0f38b5271b34bb538b0a8203e9493
lucas-paz <lucas.hpaz@outlook.com>
2021-09-04 15:38:24 -0300

    cria query de adicionar nova entidade

--- Cep/cepModels.js
+++ Cep/cepModels.js
@@ -1,9 +1,21 @@
 // Aqui executamos as queries necessaria para o CRUD
 const connection = require('../General/connection');
 
-const FIND_QUERY_CEP = 'SELECT * FROM cep_lookup.ceps WHERE cep=?';
+const FIND_QUERY_CEP = 'SELECT * FROM cep_lookup.ceps WHERE cep=?;';
+const ADD_NEW_CEP = 'INSERT INTO cep_lookup.ceps (cep, logradouro, bairro, localidade, uf)VALUES (?, ?, ?, ?, ?);';
 
 const findByCep = async (cep) => {
   const [[cepData]] = await connection.execute(FIND_QUERY_CEP, [cep]);
   return cepData;
 };
+
+const addNew = async ({cep, logradouro, bairro, localidade, uf }) => {
+  const newCepObj = {cep, logradouro, bairro, localidade, uf };
+  await connection.execute(ADD_NEW_CEP, [cep, logradouro, bairro, localidade, uf]);
+  return newCepObj;
+};
+
+module.exports = {
+  findByCep,
+  addNew,
+}
